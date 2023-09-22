fd166ce1eb5e55e9b04ffd691d230ce374ca8e2a
ParkSohyeon <bori12370@gmail.com>
2023-09-22 16:55:53 +0900

    graphQL 라이브러리 설치 및 graphQL 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { PaymentModule } from './payment/payment.module';
 import { LoggerMiddleware } from 'common/logger.middleware';
 import { HouseModule } from './house/house.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [PaymentModule, HouseModule],
+  imports: [
+    PaymentModule,
+    HouseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
