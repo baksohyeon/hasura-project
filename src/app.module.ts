8b1395937e2cdf94e345245cdb2e2c189418a412
ParkSohyeon <bori12370@gmail.com>
2023-09-22 17:06:06 +0900

    하수라 모듈 추가 및 옵션 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { LoggerMiddleware } from 'common/logger.middleware';
 import { HouseModule } from './house/house.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HasuraModule } from '@golevelup/nestjs-hasura';
+import { UserService } from './user/user.service';
+import path from 'path';
 
 @Module({
   imports: [
@@ -15,9 +18,32 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    HasuraModule.forRoot(HasuraModule, {
+      webhookConfig: {
+        /**
+         * The value of the secret Header. The Hasura module will ensure that incoming webhook payloads contain this
+         * value in order to validate that it is a trusted request
+         */
+        secretFactory: 'secret',
+        /** The name of the Header that Hasura will send along with all event payloads */
+        secretHeader: 'secret-header',
+      },
+      managedMetaDataConfig: {
+        metadataVersion: 'v3',
+        dirPath: path.join(process.cwd(), 'hasura/metadata'),
+        secretHeaderEnvName: 'HASURA_NESTJS_WEBHOOK_SECRET_HEADER_VALUE',
+        nestEndpointEnvName: 'NESTJS_EVENT_WEBHOOK_ENDPOINT',
+        defaultEventRetryConfig: {
+          intervalInSeconds: 15,
+          numRetries: 3,
+          timeoutInSeconds: 100,
+          toleranceSeconds: 21600,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
