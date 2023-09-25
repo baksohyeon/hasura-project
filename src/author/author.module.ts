29b3efa7d68ccb21bdee63c1040823c5158e8c1e
ParkSohyeon <bori12370@gmail.com>
2023-09-25 15:21:14 +0900

    graphQL 모듈 추가

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorResolver } from './author.resolver';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  providers: [AuthorResolver, AuthorService]
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  providers: [AuthorResolver, AuthorService],
 })
 export class AuthorModule {}
