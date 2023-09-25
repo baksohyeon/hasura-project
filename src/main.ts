fd85c9a52dada14f1833ed9b54cfccf5e0dcfbdd
ParkSohyeon <bori12370@gmail.com>
2023-09-25 16:30:48 +0900

    boot strap 로그 함수 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = app.get(ConfigService).get<number>('port');
+
+  await app.listen(port, () => logger.log(`Server listening on port ${port}`));
 }
 bootstrap();
